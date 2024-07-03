35fe506e6e9dac8ecdb2e90d338a73a9aeccdbb0
iamjoker021 <iamjoker021@gmail.com>
2024-07-03 19:08:34 +0530

    Add playGround logic
    Added logic to compare Rock paper scissor in playGround function

--- sandbox.js
+++ sandbox.js
@@ -5,7 +5,7 @@
 // 1. Get Compute choice
 // 2. Get Human Choice
 // 3. Right logic to play single round of Rock-Paper scissor
-// 4. Right logic to play n times
+// 4. Right logic to play n times and track score
 
 // 1. Get Computer Choice
 function getComputerChoice() {
@@ -50,5 +50,60 @@ function getHumanChoice() {
     // If none above matches ask again
     else {
         return getHumanChoice();
+    }
+}
+
+// 3. Right logic to play single round of Rock-Paper scissor
+function playGround(computerChoice, humanChoice) {
+    // Get Player1 and Player2
+    p1 = computerChoice;
+    p2 = humanChoice;
+
+    // if both player has same value
+    if (p1 === p2){
+        // tie: 0
+        return 0
+    }
+
+    // If p1 is rock
+    if (p1 === "rock"){
+        // if it is rock vs paper
+        if (p2 === "paper") {
+            // then p2
+            return 2
+        }
+        // if it is rock vs scissor
+        else {
+            // then p1
+            return 1;
+        }
+    }
+
+    // if p1 is paper
+    if (p1 === "paper") {
+        // if paper vs rock
+        if (p2 === "rock") {
+            // then p1
+            return 1;
+        }
+        // if paper vs scissor
+        else {
+            // p2
+            return 2;
+        }
+    }
+
+    // if p1 is scissor
+    if (p1 === "scissor") {
+        // if scissor vs rock
+        if (p2 === "rock") {
+            // p2
+            return 2;
+        }
+        // scissor vs paper
+        else {
+            // p1
+            return 1;
+        }
     }
 }
