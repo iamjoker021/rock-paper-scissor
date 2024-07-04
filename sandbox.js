0ada40e5e3ec1e795b653cb8ae3101668170fe29
iamjoker021 <iamjoker021@gmail.com>
2024-07-04 14:56:29 +0530

    Update logic to play round on Click
    Updated the code to play the Round on Button click and output to console

--- sandbox.js
+++ sandbox.js
@@ -25,7 +25,8 @@ function getComputerChoice() {
 
 // 2. Get Human Choice
 function getHumanChoice() {
-    let humanChoice = prompt("You(rock/paper/scissor): ");
+    let humanChoice;
+
     if (humanChoice.toLowerCase() === 'rock'){
         return 'rock';
     }
@@ -40,7 +41,7 @@ function getHumanChoice() {
     }
 }
 
-function rock_paper_scissor (p1, p2) {
+function rock_paper_scissor(p1, p2) {
     if (p1 === p2){
         return 0
     }
@@ -72,41 +73,60 @@ function rock_paper_scissor (p1, p2) {
 }
 
 // 3. Right logic to play single round of Rock-Paper scissor
-function playGround() {
-    p2 = getHumanChoice();
-    p1 = getComputerChoice();
+// function playGround() {
+//     p2 = getHumanChoice();
+//     p1 = getComputerChoice();
     
-    // Print Human and Computer Choice to display to User
-    console.log(`Human(rock/paper/scissor): ${p2}`);
-    console.log(`Computer(rock/paper/scissor): ${p1}`);
+//     // Print Human and Computer Choice to display to User
+//     console.log(`Human(rock/paper/scissor): ${p2}`);
+//     console.log(`Computer(rock/paper/scissor): ${p1}`);
     
-    // 0 - Tie, 1 - Player1Wins, 2- Player2Wins
-    return rock_paper_scissor(p1, p2)
+//     // 0 - Tie, 1 - Player1Wins, 2- Player2Wins
+//     return rock_paper_scissor(p1, p2)
     
-}
+// }
 
-function printScore(humanScore, computerScore) {
-    console.log(`humanScore: ${humanScore}\ncomputerScore: ${computerScore}`)
+function printScore(humanChoice, computerChoice, humanScore, computerScore) {
+    console.log(`humanChoice: ${humanChoice}\ncomputerChoice: ${computerChoice}\nhumanScore: ${humanScore} vs computerScore: ${computerScore}`)
 }
 
 // 4. Right logic to play n times and track score
-/*
+
 let humanScore = 0, computerScore = 0;
 const ROUND = 5;
 
-for (let i = 0; i < ROUND; i++) {
-    result = playGround();
-    // If 1 then computerWin
+// Add event lister to get humanChoice
+const choiceButton = document.querySelector('.button-options');
+choiceButton.addEventListener('click', (e) => {
+    let humanChoice = e.target.value;
+    let computerChoice = getComputerChoice();
+
+    // 0 - Tie, 1 - Computer, 2 - Human
+    let result = rock_paper_scissor(computerChoice, humanChoice);
+    
     if (result === 1) {
         computerScore += 1;
     }
-    // if 2 then humanWin
     else if (result === 2) {
         humanScore += 1;
     }
 
-    printScore(humanScore, computerScore);
-}
+    printScore(humanChoice, computerChoice, humanScore, computerScore);
+})
+
+// for (let i = 0; i < ROUND; i++) {
+//     result = playGround();
+//     // If 1 then computerWin
+//     if (result === 1) {
+//         computerScore += 1;
+//     }
+//     // if 2 then humanWin
+//     else if (result === 2) {
+//         humanScore += 1;
+//     }
+
+//     printScore(humanScore, computerScore);
+// }
 
 // Once Loop end, print the Winner
 if (computerScore > humanScore) {
@@ -119,4 +139,3 @@ else {
     console.log("TIEEEE");
 }
 
-*/
