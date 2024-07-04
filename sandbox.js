70b9ded154677f505fedd7bd49955e26a13a2894
iamjoker021 <iamjoker021@gmail.com>
2024-07-04 14:18:29 +0530

    Refactor the comments
    Instead of describing line by line, kept only required comments

--- sandbox.js
+++ sandbox.js
@@ -11,93 +11,61 @@
 function getComputerChoice() {
     // Get Random Number between 1 to 9
     let randomNum = Math.floor(Math.random() * (9 - 1 + 1)) + 1; // Got this in internet to get random min to max: Math.floor(Math.random() * (max - min + 1)) + min
-    // If number is less than or equal to 3
+
     if (randomNum <= 3) {
-        //  then rocks
         return 'rock'
     }
-    // elif number is between 3 and 6
     else if (randomNum > 3 && randomNum <= 6) {
-        // then paper
         return 'paper';
     }
-    // elif number is between 6 and 9 or greate than 6
     else if (randomNum > 6) {
-        // them scissor
         return 'scissor';
     }
 }
 
 // 2. Get Human Choice
 function getHumanChoice() {
-    // Get user input
     let humanChoice = prompt("You(rock/paper/scissor): ");
-    // Check if answere matches rock
     if (humanChoice.toLowerCase() === 'rock'){
-        // if yes, then return rock
         return 'rock';
     }
-    // Check if answer matches paper
     else if (humanChoice.toLowerCase() === 'paper'){
-        // if yes. return paper
         return 'paper';
     }
-    // Check if answer matches scissor
     else if (humanChoice.toLowerCase() === 'scissor'){
-        // if yes reutrn scissor
         return 'scissor';
     }
-    // If none above matches ask again
     else {
         return getHumanChoice();
     }
 }
 
 function rock_paper_scissor (p1, p2) {
-    // if both player has same value
     if (p1 === p2){
-        // tie: 0
         return 0
     }
 
-    // If p1 is rock
     if (p1 === "rock"){
-        // if it is rock vs paper
         if (p2 === "paper") {
-            // then p2
             return 2
         }
-        // if it is rock vs scissor
         else {
-            // then p1
             return 1;
         }
     }
-
-    // if p1 is paper
-    if (p1 === "paper") {
-        // if paper vs rock
+    else if (p1 === "paper") {
         if (p2 === "rock") {
-            // then p1
             return 1;
         }
-        // if paper vs scissor
         else {
-            // p2
             return 2;
         }
     }
-
-    // if p1 is scissor
-    if (p1 === "scissor") {
-        // if scissor vs rock
+    else if (p1 === "scissor") {
         if (p2 === "rock") {
-            // p2
             return 2;
         }
-        // scissor vs paper
         else {
-            // p1
             return 1;
         }
     }
@@ -105,7 +73,6 @@ function rock_paper_scissor (p1, p2) {
 
 // 3. Right logic to play single round of Rock-Paper scissor
 function playGround() {
-    // Get Player1 and Player2 choice
     p2 = getHumanChoice();
     p1 = getComputerChoice();
     
@@ -123,20 +90,16 @@ function printScore(humanScore, computerScore) {
 }
 
 // 4. Right logic to play n times and track score
-// Create variable humanScore for human to track score and set to 0
-// Creaete variable computerScore for computer to track score and set to 0
 let humanScore = 0, computerScore = 0;
-// Create variable ROUND for No of rounds and set it to 5
 const ROUND = 5;
-// Loop ROUND times
+
 for (let i = 0; i < ROUND; i++) {
-    // For every loop call playGround and get the value in variable called result
     result = playGround();
-    // If 1 increment computerScore by 1
+    // If 1 then computerWin
     if (result === 1) {
         computerScore += 1;
     }
-    // if 2 increment humanSCore by 1
+    // if 2 then humanWin
     else if (result === 2) {
         humanScore += 1;
     }
