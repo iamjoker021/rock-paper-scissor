2ee5db82083c35d9d8146334044ad9c4483e8f51
iamjoker021 <iamjoker021@gmail.com>
2024-07-04 15:15:39 +0530

    Refactor the code
    Removed unnecessary comments, updted the fn to more intuitive name

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,6 @@
-// Task:
-// Build Rock Paper Scissor game in Console
 
-// Sub-Tasks
-// 1. Get Compute choice
-// 2. Get Human Choice
-// 3. Right logic to play single round of Rock-Paper scissor
-// 4. Right logic to play n times and track score
-
-// 1. Get Computer Choice
+// Get Computer Choice
 function getComputerChoice() {
-    // Get Random Number between 1 to 9
     let randomNum = Math.floor(Math.random() * 3) + 1; // Got this in internet to get random min to max: Math.floor(Math.random() * (max - min + 1)) + min
 
     if (randomNum === 1) {
@@ -23,47 +14,30 @@ function getComputerChoice() {
     }
 }
 
-// 2. Get Human Choice
-function getHumanChoice() {
-    let humanChoice;
-
-    if (humanChoice.toLowerCase() === 'rock'){
-        return 'rock';
-    }
-    else if (humanChoice.toLowerCase() === 'paper'){
-        return 'paper';
-    }
-    else if (humanChoice.toLowerCase() === 'scissor'){
-        return 'scissor';
-    }
-    else {
-        return getHumanChoice();
-    }
-}
-
-function rock_paper_scissor(p1, p2) {
-    if (p1 === p2){
+// Logic for RockPaperScissor
+function rockPaperScissorLogic(player1Choice, player2Choice) {
+    if (player1Choice === player2Choice){
         return 0
     }
 
-    if (p1 === "rock"){
-        if (p2 === "paper") {
+    if (player1Choice === "rock"){
+        if (player2Choice === "paper") {
             return 2
         }
         else {
             return 1;
         }
     }
-    else if (p1 === "paper") {
-        if (p2 === "rock") {
+    else if (player1Choice === "paper") {
+        if (player2Choice === "rock") {
             return 1;
         }
         else {
             return 2;
         }
     }
-    else if (p1 === "scissor") {
-        if (p2 === "rock") {
+    else if (player1Choice === "scissor") {
+        if (player2Choice === "rock") {
             return 2;
         }
         else {
@@ -72,6 +46,7 @@ function rock_paper_scissor(p1, p2) {
     }
 }
 
+// Print the Round result to HTML
 function printScoreToHTML(round, humanChoice, computerChoice, humanScore, computerScore) {    
     const printElem = document.querySelector('ul.round-result');
 
@@ -100,19 +75,18 @@ function printScoreToHTML(round, humanChoice, computerChoice, humanScore, comput
     printElem.appendChild(list);
 }
 
-// 4. Right logic to play n times and track score
-
+// Variables to track the Game
 let humanScore = 0, computerScore = 0;
 let roundNum = 1;
 
-// Add event lister to get humanChoice
+// Add event lister to get humanChoice, trigger game on Click
 const choiceButton = document.querySelector('.button-options');
 choiceButton.addEventListener('click', (e) => {
     let humanChoice = e.target.value;
     let computerChoice = getComputerChoice();
 
-    // 0 - Tie, 1 - Computer, 2 - Human
-    let result = rock_paper_scissor(computerChoice, humanChoice);
+    // 0 - Tie, 1 - Player1/Argument1 wins the game, 2 - Player2/Argument2 wins the game
+    let result = rockPaperScissorLogic(player1Choice= computerChoice, player2Choice= humanChoice);
     
     if (result === 1) {
         computerScore += 1;
@@ -124,20 +98,6 @@ choiceButton.addEventListener('click', (e) => {
     printScoreToHTML(roundNum, humanChoice, computerChoice, humanScore, computerScore);
     roundNum++;
 })
-
-// for (let i = 0; i < ROUND; i++) {
-//     result = playGround();
-//     // If 1 then computerWin
-//     if (result === 1) {
-//         computerScore += 1;
-//     }
-//     // if 2 then humanWin
-//     else if (result === 2) {
-//         humanScore += 1;
-//     }
-
-//     printScore(humanScore, computerScore);
-// }
 
 // Once Loop end, print the Winner
 if (computerScore > humanScore) {
